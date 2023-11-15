e3bbf32b751059898bb48f15ffc04e2ae7132157
antoinech <smb.antoine@gmail.com>
2023-11-15 15:14:26 +0100

    Added Plance and Transition Adapter

---
+++ mapd-pne-code/src/org/pneditor/petrinet/adapters/PlaceAdapter.java
@@ -0,0 +1,41 @@
+package org.pneditor.petrinet.adapters;
+
+import org.pneditor.petrinet.AbstractPlace;
+import org.pneditor.petrinet.models.Place;
+
+public class PlaceAdapter extends AbstractPlace {
+	private Place place;
+
+	public PlaceAdapter(String label) {
+		super(label);
+		this.place = new Place(0);
+	}
+
+	@Override
+	public void addToken() {
+		this.place.add(1);
+		
+	}
+
+	@Override
+	public void removeToken() {
+		this.place.remove(1);
+		
+	}
+
+	@Override
+	public int getTokens() {
+		return this.place.getCountTokens();
+	}
+
+	@Override
+	public void setTokens(int tokens) {
+		if (tokens > this.getTokens()) {
+			this.place.add(tokens - this.getTokens());
+		}
+		else {
+			this.place.remove(this.getTokens() - tokens);
+		}
+	}
+
+}
---
+++ mapd-pne-code/src/org/pneditor/petrinet/adapters/TransitionAdapter.java
@@ -0,0 +1,11 @@
+package org.pneditor.petrinet.adapters;
+
+import org.pneditor.petrinet.AbstractTransition;
+
+public class TransitionAdapter extends AbstractTransition {
+
+	public TransitionAdapter(String label) {
+		super(label);
+	}
+
+}
