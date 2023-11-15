fe203e8986be2a5080313b7795f3a03d8e4ab89c
antoinech <smb.antoine@gmail.com>
2023-11-15 15:00:52 +0100

    Fixed import

--- mapd-pne-code/src/org/pneditor/petrinet/models/PetriNet.java
+++ mapd-pne-code/src/org/pneditor/petrinet/models/PetriNet.java
@@ -14,7 +14,7 @@ import org.pneditor.petrinet.models.Transition;
  * @author Cheucle
  *
  */
-public class PetriNet {
+public class PetriNet implements PetriNetwork {
 
 	/**
 	 * List containing all transitions of the network.
@@ -32,21 +32,25 @@ public class PetriNet {
 		this.listPlaces = new LinkedList<Place>();
 	}
 
+	@Override
 	public void triggerTransition(Transition transition) {
 		transition.fire();
 
 	}
 
+	@Override
 	public void add(Place place) {
 		this.listPlaces.add(place);
 
 	}
 
+	@Override
 	public void add(Transition transition) {
 		this.listTransitions.add(transition);
 
 	}
 
+	@Override
 	public void add(Place place, Transition transition, int weight, EdgeType type) {
 		Edge edge = null;
 		switch (type) {
@@ -73,12 +77,14 @@ public class PetriNet {
 		transition.addEdge(edge);
 	}
 
+	@Override
 	public void remove(Place place) {
 		this.listPlaces.remove(place);
 		// When we remove a place, we remove all edges linked to this place
 		this.listTransitions.forEach(transition -> this.remove(place, transition));
 	}
 
+	@Override
 	public void remove(Transition transition) {
 		// When removing a transition, there is no need to explicitly remove edges
 		// because edges are stored by the transition that is removed from memory by the
@@ -86,6 +92,7 @@ public class PetriNet {
 		this.listTransitions.remove(transition);
 	}
 
+	@Override
 	public void remove(Place place, Transition transition) {
 		transition.removeEdgeFromPlace(place);
 	}
