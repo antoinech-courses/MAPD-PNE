8e696cdf64c83abdca3595987f3c0982a156a0ad
nathan-claeys <127297857+nathan-claeys@users.noreply.github.com>
2023-11-15 16:17:27 +0100

    maj edgeAdapter

--- mapd-pne-code/src/org/pneditor/petrinet/adapters/EdgeAdapter.java
+++ mapd-pne-code/src/org/pneditor/petrinet/adapters/EdgeAdapter.java
@@ -13,61 +13,26 @@ import org.pneditor.petrinet.models.Transition;
 
 public class EdgeAdapter extends AbstractArc {
 	private Edge edge;
-	private TransitionAdapter transition;
-	private PlaceAdapter place;
+	private AbstractNode source;
+	private AbstractNode destination;
 	
-	public EdgeAdapter(PlaceAdapter place,TransitionAdapter transition,EdgeType edgeType) {
-		switch (edgeType) {
-		case EMPTY:
-			this.place = place;
-			this.transition = transition;
-			this.edge = new EdgeEmpty(place.getModel());
-			this.transition.getModel().addEdge(edge);
-			break;
-		case IN:
-			this.place = place;
-			this.transition = transition;
-			this.edge = new EdgeIn(place.getModel());
-			this.transition.getModel().addEdge(edge);
-			break;
-		case OUT:
-			this.place = place;
-			this.transition = transition;
-			this.edge = new EdgeOut(place.getModel());
-			this.transition.getModel().addEdge(edge);
-			break;
-		case ZERO:
-			this.place = place;
-			this.transition = transition;
-			this.edge = new EdgeZero(place.getModel());
-			this.transition.getModel().addEdge(edge);
-			break;
-		default:
-			break;
-			
-		}
+	public EdgeAdapter(AbstractNode source, AbstractNode destination, Edge edge) {
+		this.edge = edge;
+		this.source = source;
+		this.destination = destination;
 	}
 
 	@Override
 	public AbstractNode getSource() {
 		// TODO Auto-generated method stub
-		if (this.edge instanceof EdgeIn) {
-			return this.transition; 
-		}
-		else {
-			return this.place;
-		}
+		return this.source;
 	}
 
 	@Override
 	public AbstractNode getDestination() {
 		// TODO Auto-generated method stub
-		if (this.edge instanceof EdgeIn) {
-			return this.place;
-		}
-		else {
-			return this.transition; 
-		}
+		return this.destination;
+		
 	}
 
 	@Override
