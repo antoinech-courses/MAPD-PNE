78b9b7a0afd135b3e8409132317743c9e87e141e
nathan-claeys <127297857+nathan-claeys@users.noreply.github.com>
2023-11-26 16:44:13 +0100

    package-info maj

--- mapd-pne-code/src/org/pneditor/petrinet/models/cheucleclaeys/package-info.java
+++ mapd-pne-code/src/org/pneditor/petrinet/models/cheucleclaeys/package-info.java
@@ -1,5 +1,7 @@
 package org.pneditor.petrinet.models.cheucleclaeys;
 
 /**
- * A package to handle petri network calculations and to save the model we have implemented
+ * A package to handle petri network calculations and to save the model we have implemented.
+ * It is composed of all the classes that are used for the petri network (4 types of edges, place, transition, petriNetwork).
+ * And it also has one abstract class Edge, an interface PetriNetwork and an enum EdgeType to help implementation of the code.
  **/
