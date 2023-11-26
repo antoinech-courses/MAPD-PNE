ccb590e8f61439717b2a29bbb4d9807abd6f545d
nathan-claeys <127297857+nathan-claeys@users.noreply.github.com>
2023-11-26 16:41:04 +0100

    add package-info files

---
+++ mapd-pne-code/src/org/pneditor/petrinet/adapters/cheucleclaeys/package-info.java
@@ -0,0 +1,6 @@
+package org.pneditor.petrinet.adapters.cheucleclaeys;
+
+/**
+ * A package to apply the adapter pattern and to join the pne interface and our model. 
+ * It is composed of four classes to implement the four abstract classes of the pne model
+ **/
---
+++ mapd-pne-code/src/org/pneditor/petrinet/models/cheucleclaeys/package-info.java
@@ -0,0 +1,5 @@
+package org.pneditor.petrinet.models.cheucleclaeys;
+
+/**
+ * A package to handle petri network calculations and to save the model we have implemented
+ **/
